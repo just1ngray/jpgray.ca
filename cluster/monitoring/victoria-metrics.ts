3775552f5e8d13daf2279e2bb09b36f335ca258f
Justin Gray <just1ngray@outlook.com>
2026-03-20 00:28:31 -0300

    fix name too long

--- cluster/monitoring/victoria-metrics.ts
+++ cluster/monitoring/victoria-metrics.ts
@@ -25,6 +25,11 @@ export const victoriaMetricsChart = new k8s.helm.v3.Release("victoria-metrics", 
     chart: "victoria-metrics-single",
     version: "0.33.0",
     repositoryOpts: { repo: "https://victoriametrics.github.io/helm-charts/" },
+    // must explicitly set a short name because otherwise we go over the 63 character limit
+    // - pulumi will set the name to `victoria-metrics` to match above, plus a random string to deduplicate
+    // - then the helm chart adds `-victoria-metrics-single-server`
+    // - then when creating a pod, kubernetes will add another random suffix
+    name: "vm",
     namespace: ns.metadata.name,
     values: {
         server: {
